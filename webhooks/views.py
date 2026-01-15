059472b9cde0e6dada03671cb2f2ac33c85b461f
Verma <av185228@ncr.com>
2026-01-14 22:06:24 -0500

    first commit

---
+++ webhooks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+
+# Create your views here.
+def testing(request):
+    print("Method: ", request.method)
+    print("Path:", request.path)
+    print("GET params:", request.GET.dict())   
+    print("Headers:", dict(request.headers))   
+    print("Body (raw):", request.body.decode('utf-8', errors='ignore'))  
+    data = {
+        "time": timezone.localtime(timezone.now()),
+        "method" : request.method,
+        "path": request.path,
+        "get params": request.GET.dict(),
+        "headers": dict(request.headers)
+    }
+    print(data.items())
+    return render(request, "index.html", {"data": data})
